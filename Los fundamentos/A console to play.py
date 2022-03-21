9c5de409e99ec2baa7e3ba37345047e1fba27f25
Matías Inostroza <m.a.i.c.1998@outlook.com>
2022-03-21 17:02:55 -0300

    listo

--- Los fundamentos/A console to play.py
+++ Los fundamentos/A console to play.py
@@ -1,13 +1,24 @@
-
-dojo = {
-   'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
-   'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
-}
-def imprimiendo_dict(dict):
-    for llave in dict:
-        print(f"{len(dict[llave])} {llave}")
-        for dato in dict[llave]:
-            print(f"{dato}")
-imprimiendo_dict(dojo)
-
+# Function to do insertion sort
+def insertionSort(arr):
+ 
+    # Traverse through 1 to len(arr)
+    for i in range(1, len(arr)):
+ 
+        key = arr[i]
+ 
+        # Move elements of arr[0..i-1], that are
+        # greater than key, to one position ahead
+        # of their current position
+        j = i-1
+        while j >= 0 and key < arr[j] :
+                arr[j + 1] = arr[j]
+                j -= 1
+        arr[j + 1] = key
+ 
+ 
+# Driver code to test above
+arr = [12, 11, 13, 5, 6]
+insertionSort(arr)
+for i in range(len(arr)):
+    print ("% d" % arr[i])
 
